4c751761fde14484d1c4e199fca5af1198a5d31d
ksjfaskjcnjdnjkcansdkcnlkscn <157131767+ksjfaskjcnjdnjkcansdkcnlkscn@users.noreply.github.com>
2024-05-08 17:25:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# secret-python-xhapgu
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ksjfaskjcnjdnjkcansdkcnlkscn/secret-python-xhapgu)
---
+++ main.py
@@ -0,0 +1,12 @@
+import platform
+
+name = input("enter name: ")
+print("welcome to my website " + name)
+print("why are you here " + name)
+question1 = input("Why?: ") 
+if question1 == "I made you":
+  if name == "Mokka":
+    print("welcome creator.")
+  else:
+    print("oh... interesting.")
+print("well have a look around if you want.")
